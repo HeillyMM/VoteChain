9aeba95928fce8f49c7ccec2ca86a76d8716e9b6
Heilly <helenkeylli@gmail.com>
2026-06-01 08:37:35 -0400

    Ruta ciudadano/ci

--- segip/app.py
+++ segip/app.py
@@ -9,20 +9,27 @@ app.config.from_object(Config)
 
 db.init_app(app)
 
-@app.route("/ciudadanos",methods=['GET'])
+@app.route("/ciudadano/<ci>", methods=["GET"])
 @require_api_key
-def get_ciudadanos():
-    ciudadanos = Ciudadano.query.filter(Ciudadano.activo==True).all()
-    resultado = []
-    for ciudadano in ciudadanos:
-        resultado.append({
-            "ci":ciudadano.ci,
-            "nombres":ciudadano.nombres,
-            "apellido_paterno":ciudadano.apellido_paterno,
-            "apellido_materno":ciudadano.apellido_materno
-            })
+def verificar_ci(ci):
 
-    return jsonify(resultado),200
+    ciudadano = Ciudadano.query.filter_by(ci=ci, activo=True).first()
+
+    if not ciudadano:
+        return jsonify({
+            "valido": False,
+            "mensaje": "CI no encontrado"
+        }), 404
+
+    return jsonify({
+        "valido": True,
+        "ci": ciudadano.ci,
+        "nombres": ciudadano.nombres,
+        "apellido_paterno": ciudadano.apellido_paterno,
+        "apellido_materno": ciudadano.apellido_materno,
+        "departamento": ciudadano.departamento.nombre,
+        "vivo": ciudadano.vivo
+    }), 200
 
 if __name__ == "__main__":
     app.run(port=5001, debug=True)
