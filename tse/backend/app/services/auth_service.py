2ab92fc71429d071680ed39b6593452b3a469690
Limber <limachiquispelimber@gmail.com>
2026-06-01 20:16:44 -0400

    Auth_service modificado para que sea compatible con auth.py

--- tse/backend/app/services/auth_service.py
+++ tse/backend/app/services/auth_service.py
@@ -1,104 +1,124 @@
+from flask import session
+from app.extensions import bcrypt, db
 from app.models.usuario import Usuario
-from flask_bcrypt import check_password_hash
-from app.extensions import db
 
 class AuthService:
 
     @staticmethod
     def autenticar(email, password):
-        user = Usuario.query.filter_by(email=email).first()
 
-        if not user:
-            return None
-
-        if not check_password_hash(user.password_hash, password):
-            return None
-
-        return user
-    
-from flask import session
-
-from app.extensions import bcrypt
-from app.models.usuario import Usuario
-
-class AuthService:
-    @staticmethod
-    def login(email: str, password: str) -> tuple[bool, str]:
         usuario = Usuario.query.filter_by(
-            email = email,
-            activo = True
+            email=email,
+            activo=True
         ).first()
-        
+
         if not usuario:
-            return False, "Usuario no encontrado"
-        
-        if not bcrypt.check_password_hash(usuario.password_hash, password):
-            return False, "Contrasenha incorrecta"
-        
+            return None
+
+        if not bcrypt.check_password_hash(
+            usuario.password_hash,
+            password
+        ):
+            return None
+
+        return usuario
+
+    @staticmethod
+    def login(email: str, password: str):
+
+        usuario = AuthService.autenticar(
+            email,
+            password
+        )
+
+        if not usuario:
+            return False, "Credenciales incorrectas"
+
         session["user_id"] = usuario.id
         session["rol_id"] = usuario.rol_id
         session["nombre"] = usuario.nombres
-        
+
         return True, "Login exitoso"
-    
+
     @staticmethod
     def usuario_actual():
-        user_id = session.get("id") #si no funciona cambiar por user_id "id"
-        if not user_id: 
+
+        user_id = session.get("user_id")
+
+        if not user_id:
             return None
-        
+
         return Usuario.query.get(user_id)
-    
+
     @staticmethod
-    def autenticado()-> bool:
+    def autenticado():
         return "user_id" in session
-    
+
     @staticmethod
-    def tiene_rol(*roles) -> bool:
+    def tiene_rol(*roles):
+
         usuario = AuthService.usuario_actual()
-        
+
         if not usuario:
             return False
+
         return usuario.rol.nombre in roles
-    
+
     @staticmethod
-    def crear_usuario(ci, nombres, apellidos, email, password, rol_id):
+    def crear_usuario(
+        ci,
+        nombres,
+        apellidos,
+        email,
+        password,
+        rol_id
+    ):
+
         existe = Usuario.query.filter(
-            (Usuario.email == email) | 
+            (Usuario.email == email) |
             (Usuario.ci == ci)
         ).first()
-        
-        
+
         if existe:
-            raise ValueError("ya existe un usuario con ese CI o Email")
+            raise ValueError(
+                "Ya existe un usuario con ese CI o Email"
+            )
+
         password_hash = bcrypt.generate_password_hash(
             password
         ).decode("utf-8")
-        
+
         usuario = Usuario(
             ci=ci,
             nombres=nombres,
             apellidos=apellidos,
-            email = email,
-            password_hash = password_hash,
-            rol_id = rol_id
+            email=email,
+            password_hash=password_hash,
+            rol_id=rol_id
         )
-        
-        
+
         db.session.add(usuario)
         db.session.commit()
-        
+
         return usuario
-    
+
     @staticmethod
-    def cambiar_password(usuario_id:int, nueva_password:str):
+    def cambiar_password(
+        usuario_id,
+        nueva_password
+    ):
+
         usuario = Usuario.query.get(usuario_id)
+
         if not usuario:
-            raise ValueError("usuario no encontrado")
-        
+            raise ValueError(
+                "Usuario no encontrado"
+            )
+
         usuario.password_hash = bcrypt.generate_password_hash(
             nueva_password
-        ).decode('utf-8')
-        
+        ).decode("utf-8")
+
         db.session.commit()
+
         return True
